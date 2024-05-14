0ab16d04e8fcd50bae0dda78467b49e3e4a0f4de
Sai Ram Krishna <105543330+Saikrish1408@users.noreply.github.com>
2024-05-14 22:52:16 +0530

    14 / MAY / 2024

---
+++ MAY142024.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class DailyChallenge14052024 {
+    public static void main(String[] args) {
+        Scanner scannerObject = new Scanner(System.in);
+        int n = scannerObject.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++)
+            arr[i] = scannerObject.nextInt();
+        System.out.println(alternativeOddOrEven(n, arr, arr[0]%2 == 0, arr[0]%2 != 0));
+        recursiveApproach(arr, n, arr[0]%2 == 0, arr[0]%2 != 0);
+        scannerObject.close();
+    }
+
+    public static String alternativeOddOrEven(int n, int[] arr, boolean isEven, boolean isOdd) {
+        for(int i : arr) {
+            if((i % 2 == 0) && (isEven) && (!isOdd)) {
+                isEven = false;
+                isOdd = true;
+            } else if ((i % 2 != 0) && (!isEven) && (isOdd)) {
+                isEven = true;
+                isOdd = false;
+            } else {
+                return "no";
+            }
+        }
+        return "yes";
+    }
+}
